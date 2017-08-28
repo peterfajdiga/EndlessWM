1f28a9162f6323cb2864dc5a3ff1af59bdb4e6cb
Peter Fajdiga <peter.fajdiga@gmail.com>
2017-08-28 22:13:23 +0200

    layout on request_geometry

--- src/endlesswm.c
+++ src/endlesswm.c
@@ -45,6 +45,21 @@ static void view_destroyed(wlc_handle view) {
     }
 }
 
+static void view_request_move(wlc_handle view, const struct wlc_point* origin) {
+    fprintf(stderr, "Request move view %d\n", view);
+}
+
+static void view_request_resize(wlc_handle view, uint32_t edges, const struct wlc_point* origin) {
+    fprintf(stderr, "Request resize view %d\n", view);
+}
+
+static void view_request_geometry(wlc_handle view, const struct wlc_geometry* g) {
+    if (isGriddable(view)) {
+        assert (getWindow(view) != NULL);
+        layoutGrid(getWindow(view)->parent->parent);
+    }
+}
+
 static void view_focus(wlc_handle view, bool focus) {
     wlc_view_set_state(view, WLC_BIT_ACTIVATED, focus);
 }
@@ -53,13 +68,16 @@ int main(int argc, char *argv[]) {
     readConfig();
     grid_init();
     
-    wlc_set_view_created_cb     (&view_created);
-    wlc_set_view_destroyed_cb   (&view_destroyed);
-    wlc_set_view_focus_cb       (&view_focus);
-    wlc_set_keyboard_key_cb     (&keyboard_key);
-    wlc_set_pointer_button_cb   (&pointer_button);
-    wlc_set_pointer_motion_cb_v2(&pointer_motion);
-    wlc_set_pointer_scroll_cb   (&pointer_scroll);
+    wlc_set_view_created_cb         (&view_created);
+    wlc_set_view_destroyed_cb       (&view_destroyed);
+    wlc_set_view_focus_cb           (&view_focus);
+    wlc_set_keyboard_key_cb         (&keyboard_key);
+    wlc_set_pointer_button_cb       (&pointer_button);
+    wlc_set_pointer_motion_cb_v2    (&pointer_motion);
+    wlc_set_pointer_scroll_cb       (&pointer_scroll);
+    wlc_set_view_request_move_cb    (&view_request_move);
+    wlc_set_view_request_resize_cb  (&view_request_resize);
+    wlc_set_view_request_geometry_cb(&view_request_geometry);
 
     if (!wlc_init())
         return EXIT_FAILURE;
